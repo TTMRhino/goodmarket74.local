ad238a5ad34e1ed487a43b34ad6f220d04f8dbb5
TTMRhino <darkrino@mail.ru>
2022-03-17 17:08:25 +0500

    Update db.php

--- config/db.php
+++ config/db.php
@@ -2,7 +2,7 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=u28496_test',
+    'dsn' => 'mysql:host=localhost;dbname=',
     'username' => 'root',
     'password' => '',
     'charset' => 'utf8',
